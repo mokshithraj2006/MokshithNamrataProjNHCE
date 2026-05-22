5c657c7b3ca5eca7763c9d1e4f0fbbfd9dd24d5d
Mokshith Raj Dasari <88304893+mokshithraj2006@users.noreply.github.com>
2026-05-22 17:57:25 +0530

    fixed render port

--- app.py
+++ app.py
@@ -228,3 +228,9 @@ if __name__ == "__main__":
     os.makedirs(UPLOAD_DIR, exist_ok=True)
     init_db()
     app.run(debug=True)
+
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
